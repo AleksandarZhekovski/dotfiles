c4f62e6edcf883ebc732ecfe2b650caeb390c9c3
AleksandarZhekovski <aleksandarzhekovski@protonmail.com>
2024-07-03 15:06:31 +0300

    Initial comit

---
+++ hypr/audioswitch.zsh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+
+# Your headphones name (or keyword)
+# and another device you want switch to (or keyword)
+#
+# You can find these names (or keywords) by running this command:
+#    wpctl status | grep -A3 -m 1 Sinks | tail -n 3 |
+#
+#	EXAMPLE:
+#	headphonesName=HyperX
+# 	anotherDevice=HDMI
+
+headphonesName=Kraken
+anotherDevice=HDMI
+
+# Getting headphones sink id
+headphones=$(wpctl status | grep -A3 -m 1 Sinks | tail -n 3 | grep $headphonesName | grep -oP '^[^0-9]*\K[0-9]+')
+
+# Getting another device sink id
+device=$(wpctl status | grep -A3 -m 1 Sinks | tail -n 3 | grep $anotherDevice | grep -oP '^[^0-9]*\K[0-9]+')
+
+if [[ "$(wpctl status | grep -A3 -m 1 Sinks | tail -n 3 | grep "\*")" = *"$headphonesName"* ]]; then
+	let nonActive=$device
+	nonActiveName=$anotherDevice
+	nonActiveIcon=speaker
+else
+	let nonActive=$headphones
+	nonActiveName=$headphonesName
+	nonActiveIcon=headphones
+fi
+
+# Changing output
+wpctl set-default $nonActive &&
